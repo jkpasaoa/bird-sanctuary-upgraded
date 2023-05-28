a3e6d87a0dd75906c7337393033e36ec0a10caeb
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-28 10:59:04 -0400

    updated button CSS

--- src/Components/Birds.js
+++ src/Components/Birds.js
@@ -1,22 +1,24 @@
+import "../index.css"
+
 function Birds(props) {
   const { name, amount, img, id, cart, setCart } = props;
-    return (
-      <div className="birds">
-        <h4>{name}</h4>
-        <p>${amount}</p>
-        <img
-          src={img}
-          alt={name}
-          width="125"
-          height="100"
-        />
-        <br />
-        <button value={id} onClick={() => setCart([...cart, { name, amount }])}>
-        🪺 Adopt
-        </button>
-      </div>
-    );
-  };
-  
-  export default Birds;
-  
+  return (
+    <div className="birds">
+      <h4>{name}</h4>
+      <p>${amount}</p>
+      <img
+        src={img}
+        alt={name}
+        width="125"
+        height="100"
+      />
+      <br />
+      <button className="button" value={id} onClick={() => setCart([...cart, { name, amount }])}>
+        <span>🪺 Adopt</span>
+      </button>
+
+    </div>
+  );
+};
+
+export default Birds;
