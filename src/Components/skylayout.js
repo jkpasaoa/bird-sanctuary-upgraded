7e1ebb3416b73ffa77c21f927387fd0d38a75e24
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-28 21:17:18 -0400

    added inline into skylayout to see background change on netlify be applied

--- src/Components/skylayout.js
+++ src/Components/skylayout.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import '../index.css';
+import backgroundImage from '../assets/trees.jpeg';
 
 function SkyLayout() {
+  const skyBackgroundStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    zIndex: -1,
+    opacity: 0.65,
+  };
+
   return (
-    <div className="sky-background">
-    </div>
+    <div style={skyBackgroundStyle}></div>
   );
 }
 
